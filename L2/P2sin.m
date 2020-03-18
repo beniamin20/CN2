9aa84b1721b466607e31f622ee9191a0d02cd2bf
Victor Todoran <victor.todoran@evozon.com>
2020-03-18 22:45:15 +0200

    Add script that computes the value of sin(x) using a Taylor Series

---
+++ L2/P2sin.m
@@ -0,0 +1,13 @@
+% Dezvoltarea sinusului in serie Taylor.
+function result = P2sin(x = 10 * pi, err = eps)
+  result = 0;
+  % reducerea inputului la primul cadran
+  x = mod(x, 2 * pi);
+  term = x;
+  n = 1;
+  do 
+    result += term;
+    term = (-1)^n * x^(2*n + 1) / factorial(2 * n + 1);
+    n++;
+  until (abs(term) < err)
+endfunction
