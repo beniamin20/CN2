9cb4bff62a63d103022e2b4a91359729b8ba3ac8
Victor Todoran <victor.todoran@evozon.com>
2020-03-21 15:03:44 +0200

    Add expected values to testAtoLUP

--- L4/testAtoLUP.m
+++ L4/testAtoLUP.m
@@ -2,4 +2,7 @@
 function testAtoLUP()
   A = [10 -7 0; -3 2 6; 5 -1 5];
   [L,U,P] = AtoLUP(A)
+  expected_U = [10 -7 0; 0 2.5 5; 0 0 6.2]
+  expected_L = [1 0 0; -0.5 1 0; 0.3 0.4 1]
+  expected_P = [1 0 0; 0 0 1; 0 1 0]
 endfunction
