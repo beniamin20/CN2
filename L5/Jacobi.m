6e0e21bb92aa252e3fa57db56450f78394676bb8
Victor Todoran <victor.todoran@evozon.com>
2020-03-30 21:45:03 +0300

    Add script that computes A*x = b using the Jacbi method. Add resources that explain the Jacobi method.

---
+++ L5/Jacobi.m
@@ -0,0 +1,23 @@
+function x = Jacobi(A,b, err = eps)
+  % sanity check
+  MAX_ITERATIONS = 1000;
+  current_iteration = 0;
+  % n - no of ecuations
+  n = size(A,1);
+  % x - initial aproximation
+  x_previous = x_curent = zeros(n,1);
+  % M - diagonal matrix
+  M = diag(diag(A));
+  % N - 0 on the diagonal, -A in rest
+  N = M - A;
+  
+  do
+    current_iteration++;
+    x_previous = x_curent;
+    x_curent   = M\(N*x_previous+b);
+   
+    err_curent = norm(x_curent-x_previous,inf)/norm(x_curent);
+  until(err_curent < err || current_iteration > MAX_ITERATIONS)
+  
+  x = x_previous;
+endfunction
---
+++ L5/TestJacobi.m
@@ -0,0 +1,6 @@
+% Test Jacobi implementation
+A = [2 0 1; 0 2 1; 0 1 2];
+b = [6; 3; 4.5];
+
+expected_x = [2; 0.5; 2]
+x = Jacobi(A,b)
