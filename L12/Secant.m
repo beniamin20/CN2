2c513c01f66b446e0731ee7840670ad85a03b3f8
Victor Todoran <victor.todoran@yahoo.com>
2020-05-13 13:57:14 +0300

    Refactor L12/Secant

--- L12/Secant.m
+++ L12/Secant.m
@@ -5,7 +5,7 @@
 % nmax = 100
 function result = Secant(f,x1,x2,err,nmax)
   for i = 1:nmax
-    result = x2 - ((x2 - x1) * f(x2)) / (f(x2) - f(x1));
+    result = x2 - ((x2 - x1) .* f(x2)) ./ (f(x2) - f(x1));
     if abs(result - x2) < err + err * abs(result)
       return;
     end
